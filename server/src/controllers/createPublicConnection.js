d5531b488498e5fbc5992f8ef3bc63969d3cc0af
dscrane <daegancrane@gmail.com>
2021-10-07 11:49:08 -0400

    overall rework for creating connection and initial data fetching

---
+++ server/src/controllers/createPublicConnection.js
@@ -0,0 +1,37 @@
+import { Chatroom, User } from "../models/index.js";
+
+export const createPublicConnection = async (
+  socket,
+  chatroomName,
+  userId,
+  cb
+) => {
+  try {
+    const user = await User.findById(userId);
+    if (user === null) {
+      throw new DatabaseError("User not found");
+    }
+    const chat = new Chatroom({
+      name: chatroomName,
+      createdBy: userId,
+      currentMembers: [userId],
+    });
+    user.currentRooms = [...user.currentRooms, chatroomName];
+    user.createdRooms = [...user.createdRooms, chatroomName];
+
+    await chat.save();
+    socket.emit("chatroom-created", chat, user.name);
+    cb(["create-chatroom", chatroomName, "complete"]);
+
+    await user.save();
+  } catch (e) {
+    if (e.code === 11000) {
+      console.log(new DatabaseError("Duplicate room name", e.keyValue.name));
+      socket.emit(
+        "error",
+        new DatabaseError("Duplicate room name", e.keyValue.name)
+      );
+    }
+    socket.emit("error", { ...e });
+  }
+};
